dcfd341b0e275200086c912325132ee2644c2047
yvonneventure <103771536+yvonneventure@users.noreply.github.com>
2022-04-23 23:41:31 -0400

    Update main.py

--- 020 Intermediate Python/Day 38 Project - Workout Tracker Use Google Sheet/main.py
+++ 020 Intermediate Python/Day 38 Project - Workout Tracker Use Google Sheet/main.py
@@ -1,6 +1,6 @@
 ENDPOINT="https://trackapi.nutritionix.com/v2/natural/exercise"
-API_Key="af3eff5f4c31b97d6685d0f1410fb5b5"
-API_ID="1d51e05e"
+API_Key=KEY
+API_ID=ID
 
 heads={
     "x-app-id":API_ID,
@@ -15,7 +15,7 @@ response=requests.post(url=ENDPOINT,json=params,headers=heads)
 result=response.json()
 print(result)
 
-SHEETY="https://api.sheety.co/6e836f5a587a97a40c865d7bc1fe52fc/workoutTacker/workouts"
+SHEETY=f"https://api.sheety.co/{API_KEY}/workoutTacker/workouts"
 
 
 today_date = datetime.datetime.now().strftime("%d/%m/%Y")
