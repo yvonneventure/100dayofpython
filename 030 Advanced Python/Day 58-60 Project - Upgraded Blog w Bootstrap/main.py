9c878abc868fc3d3f1b2be722b1db5fff568a07a
yvonneventure <103771536+yvonneventure@users.noreply.github.com>
2022-05-24 05:52:36 -0400

    Add files via upload

---
+++ 030 Advanced Python/Day 58-60 Project - Upgraded Blog w Bootstrap/main.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request
+import requests
+import smtplib
+
+posts = requests.get("https://api.npoint.io/d44977d166848e75b7d1").json()
+OWN_EMAIL = myemail
+OWN_PASSWORD = mypassword
+print(posts)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html")
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
